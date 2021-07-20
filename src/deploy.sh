79ebe36c552c2ddbdee6d689a37641bbd515c801
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-07-20 14:43:12 +0100

    Reworked deploy script to work with new changes.

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,16 +2,26 @@
 # Script to deploy the installer
 
 # Repository link and files
-REPO_LINK="https://raw.githubusercontent.com/pedroclobo/arch/main/src/"
-files=("install.sh" "library.sh" "stdin.sh")
+MAIN_REPO_LINK="https://raw.githubusercontent.com/pedroclobo/arch/main/src/"
+main_files=("install.sh")
+
+LIB_REPO_LINK="https://raw.githubusercontent.com/pedroclobo/arch/main/src/lib/"
+lib_files=("disk.sh" "package.sh" "stdin.sh" "system.sh")
+
 
 # Dependency packages
 pacman -Syy wget --noconfirm >/dev/null 2>&1
 
 # Download the files and make them executable
-for file in "${files[@]}"
+for file in "${main_files[@]}"
 do
-	wget -q "${REPO_LINK}""$file"
+	wget -q "${MAIN_REPO_LINK}""$file"
+	chmod +x "$file"
+done
+
+for file in "${lib_files[@]}"
+do
+	wget -q "${LIB_REPO_LINK}""$file"
 	chmod +x "$file"
 done
 
